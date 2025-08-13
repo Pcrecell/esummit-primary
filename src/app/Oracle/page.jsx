02be4eac0da3bce079c8a3a759727fd6b650f3a4
Abinash Mohanty <abinashmohanty8059@gmail.com>
2025-08-13 16:37:43 +0530

    Oracle page added

---
+++ src/app/Oracle/page.jsx
@@ -0,0 +1,7 @@
+import Functionname from "@/components/esummit/Oracle/Oracle"
+
+export default function Oracle() {
+  return (
+    <Functionname/> // function name to be modified if needed
+  )
+}
