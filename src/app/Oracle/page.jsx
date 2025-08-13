9cee85cd03380fc24ccf7aa350fdc5c4abddbeed
rupanjanas <saharupanjana04@gmail.com>
2025-08-13 19:16:47 +0530

    Update

--- src/app/Oracle/page.jsx
+++ src/app/Oracle/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/esummit/Oracle/Hero";
 import Story from "@/components/esummit/Oracle/Story";
+import Stages from "@/components/esummit/Oracle/Stages";
 
 export default function Oracle() {
   return (
     <>
       <Hero />
       <Story />
+      <Stages/>
     </>
   );
 }
