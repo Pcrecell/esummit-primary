6803f2d2962754cbc6100762baf656e3a24f15ab
Sujay kumar <sujayraw13@gmail.com>
2025-08-17 21:05:10 +0530

    temp rename

--- src/app/AIF/page.jsx
+++ src/app/AIF/page.jsx
@@ -1,13 +1,9 @@
 // Import AIF code from @/components/AIF to over here
-"use client"
+"use client";
 
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Aif from "@/components/esummit/aif/Aif";
 
-
 export default function HomePage() {
-
-    return ( 
-    <Aif />
-    )
+  return <Aif />;
 }
